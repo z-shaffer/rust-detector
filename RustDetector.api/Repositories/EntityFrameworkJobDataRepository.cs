5f3ddbb0f295d3f44677f7a4d1375cd78b5a0932
z-shaffer <zshaffer@protonmail.com>
2024-01-28 23:00:26 +0100

    Caching implementation

--- RustDetector.api/Repositories/EntityFrameworkJobDataRepository.cs
+++ RustDetector.api/Repositories/EntityFrameworkJobDataRepository.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using RustDetector.api.Data;
 using RustDetector.api.Entities;
 
@@ -6,9 +7,19 @@ namespace RustDetector.api.Repositories;
 
 public class EntityFrameworkJobDataRepository(JobDataContext dbContext) : IJobDataRepository
 {
+    private readonly IMemoryCache _cache;
+    private readonly TimeSpan _cacheExpiration = GetDaysUntilEndOfMonth();
+    private readonly string DataKey = "JobDataCache";
+    
     public async Task<IEnumerable<JobData>> GetAllAsync()
     {
-        return await dbContext.JobDataSet.AsNoTracking().ToListAsync();
+        if (!_cache.TryGetValue(DataKey, out IEnumerable<JobData> cachedData))
+        {
+            cachedData = await dbContext.JobDataSet.AsNoTracking().ToListAsync();
+            _cache.Set(DataKey, cachedData, DateTime.Now.Add(_cacheExpiration));
+        }
+
+        return cachedData;
     }
 
     public async Task<JobData?> GetAsync(int id)
@@ -32,5 +43,15 @@ public class EntityFrameworkJobDataRepository(JobDataContext dbContext) : IJobDa
     {
         await dbContext.JobDataSet.Where(jobData => jobData.Id == id)
             .ExecuteDeleteAsync();
+    }
+    
+    private static TimeSpan GetDaysUntilEndOfMonth()
+    {
+        DateTime currentDate = DateTime.Now;
+        DateTime firstDayOfNextMonth = new DateTime(currentDate.Year, currentDate.Month, 1).AddMonths(1);
+        DateTime lastDayOfCurrentMonth = firstDayOfNextMonth.AddDays(-1);
+        TimeSpan daysUntilEndOfMonth = lastDayOfCurrentMonth - currentDate;
+
+        return daysUntilEndOfMonth;
     }
 }
